17400c66457dfd44cd853403aa9e3c17be3fcb68
Enrique Padilla <ultrared808@gmail.com>
2023-01-11 02:10:51 -0600

    Cursos servicios

--- src/app/modules/cursos/cursos-dialogo/cursos-dialogo.component.ts
+++ src/app/modules/cursos/cursos-dialogo/cursos-dialogo.component.ts
@@ -1,8 +1,6 @@
 import { CursosService } from 'src/app/services/cursos.service';
 import { Component } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-cursos-dialogo',
   templateUrl: './cursos-dialogo.component.html',
